6b9c0b95e6341b9ec3ba39c8b9e8736a0bd23491
StevenGolovkine <steven_golovkine@icloud.com>
2024-04-04 18:30:52 +0100

    Skeleton

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pyspline'
-copyright = '2024, Steven Golovkine'
-author = 'Steven Golovkine'
-release = 'v0.0.0'
+project = "pyspline"
+copyright = "2024, Steven Golovkine"
+author = "Steven Golovkine"
+release = "v0.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
