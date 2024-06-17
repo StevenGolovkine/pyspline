36af90efcb760076e86c1d8deedbe841ba5e28e4
StevenGolovkine <steven_golovkine@icloud.com>
2024-06-17 18:48:48 +0200

    Doc

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@ release = "v0.0.0"
 
 extensions = [
     "sphinx.ext.autodoc",
+    "numpydoc",
     "sphinx.ext.coverage",
     "sphinx.ext.napoleon",
     "sphinx.ext.mathjax",
@@ -37,3 +38,22 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_permalinks_icon = "<span>#</span>"
 html_theme = "sphinxawesome_theme"
 html_static_path = ["_static"]
+
+
+sphinx_gallery_conf = {
+    # path to your examples scripts
+    "examples_dirs": "../examples",
+    # path where to save gallery generated examples
+    "gallery_dirs": "auto_examples",
+    "reference_url": {
+        # The module you locally document uses None
+        "pyspline": None,
+    },
+    "backreferences_dir": "backreferences",
+    "doc_module": "pyspline",
+}
+
+autosummary_generate = True
+autodoc_member_order = "bysource"
+
+autodoc_default_options = {"show-inheritance": True}
